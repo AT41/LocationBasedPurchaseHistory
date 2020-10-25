3b240f9e674507475c61152d75fdfd62891fc351
Sean <sw92410@hotmail.com>
2020-10-24 18:27:48 -0600

    Add signIn & dashboard activities and their logic, remove GoogleSignInActivity files, add kotlin extension,

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -3,16 +3,46 @@ package com.example.myapplication
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
 import android.view.View
 import android.widget.EditText
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.FirebaseUser
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
 
 const val MSG = "E";
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var auth: FirebaseAuth
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+
+        auth = Firebase.auth
     }
+
+    override fun onStart() {
+        super.onStart()
+
+        val currentUser = auth.currentUser
+        handleUser(currentUser)
+    }
+
+    private fun handleUser(user: FirebaseUser?) {
+        if (user != null) {
+            val dashboardIntent = Intent(this, DashboardActivity::class.java)
+            startActivity(dashboardIntent)
+        }
+        else {
+            val signInIntent = Intent(this, SignInActivity::class.java)
+            startActivity(signInIntent)
+        }
+    }
+
     fun sendMessage(view: View) {
         val editText = findViewById<EditText>(R.id.editTextTextPersonName)
         val message = editText.text.toString()
